9f69b38de2089a78bb61a2a49f6a78a4d3c4225c
Samuel Lord <sllord@icloud.com>
2016-08-30 20:42:51 -0500

    Create cheat.js

---
+++ cheat.js
@@ -0,0 +1,11 @@
+cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
+  alert('Voilà!');
+});
+cheet('n o c l i p', function () {
+  alert('Noclip enabled');
+});
+
+cheet('s p a n i s h', function () {
+  alert('Spanish mode enabled!');
+  window.location="http://sllord-tk.github.io/spanish.mp3";
+});
